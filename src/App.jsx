9cc2b189208aceedecb158af825c4c333012d457
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2025-05-31 22:57:49 +0200

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,28 @@ const App = () => {
   }, []);
 
   return (
-    <div className="bg-purple-200 md:from-blue-300 md:via-white md:to-pink-300">
-      {/* Main content */}
+    <div
+      className="overflow-hidden bg-gradient-to-b from-pink-100 to-pink-300  md:from-blue-300 md:via-white md:to-pink-300"
+      style={{
+        minHeight: "100dvh",
+        overflow: "hidden",
+      }}
+    >
       <div className="flex-grow flex flex-col items-center justify-start px-4">
-        <h1 className="text-center text-4xl cherry mt-4 mb-2 md:mb-4">
-          Floppy Flap <br /> The Game
+        <h1 className="text-center text-2xl md:text-4xl cherry mt-4 mb-2 md:mb-4">
+          Floppy Flap
         </h1>
 
         <ResponsiveGame highScore={highScore} setHighScore={setHighScore} />
 
-        <p className="text-center text-lg text-gray-800 mt-4 mb-2 px-4 cherry">
+        <p className="text-center text-md md:text-lg text-gray-800 mt-4 mb-2 px-4 cherry">
           Tap or press SPACE to flap
         </p>
-        <p className="mt-1 mb-2 font-bold text-pink-700 text-2xl cherry">
+        <p className="mt-1 mb-2 font-bold text-pink-700 text-lg md:text-2xl cherry">
           Your Highscore: {highScore}
         </p>
       </div>
 
-      {/* Footer */}
       <footer className="text-xs text-gray-500 text-center px-4 py-4">
         <a
           href="https://github.com/LineSvensen"
